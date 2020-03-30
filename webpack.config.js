71a8a05ea9f855abc803e03f778d32dc1c8704db
Neha.Singh <neha.singh1@1MG-LPTP-970.local>
2020-03-30 21:01:48 +0530

    react boilerplate with webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  mode: 'devlopment',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: [ "style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
