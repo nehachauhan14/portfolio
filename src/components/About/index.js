becbe518a8f992df1f2ecbf3d9c28661f4371eff
Neha.Singh <neha.singh1@1MG-LPTP-970.local>
2020-05-04 00:21:28 +0530

    Added contact me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ const About = props => {
         <section id="about" className="aboutSection">
             <h1 className="title">Neha Chauhan</h1>
             <img src="src/assets/images/cartoon2.jpeg" className="cartoon" alt="me" />
-            <p class="line-1 anim-typewriter">I am a Web Developer & Dog lover... </p>
+            <p className="line-1 anim-typewriter">I am a Web Developer & Dog lover... </p>
             <div className="intro">I design and code beautifully simple things, and I love what I do.</div>
         </section>
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,17 @@ import "./Contact.scss";
 const Contact = props => {
     return (
         <section id="contact" className="contactSection">
-           My Email Address is nehachauhan79@gmail.com!!!!
+          <h1 className="contactTitle">Get in touch</h1>
+          <p className="contactSubTitle">If you wanna get in touch, talk to me about a project collaboration or just say hi,
+            <br />
+            send an email to
+            <span className="email">
+              <a href="mailto:nehachauhan79@gmail.com">nehachauhan79@gmail.com</a>
+            </span>
+            and ~let's talk.
+          </p>
+          <img src="src/assets/images/dog.gif" alt="dog lover" className="dogImg" />
+          <div className="quote">Living, learning, & leveling up one day at a time.</div>
         </section>
 
     )
